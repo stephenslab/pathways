dd497c7dae754e86b9f5946fc2eb614f5745fbec
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-03-16 12:12:52 -0500

    Implemented function read_hgnc_data to read the HGNC gene data.

--- code/read_data.R
+++ code/read_data.R
@@ -1,9 +1,23 @@
-# Read the pathway meta daata (pathway names and data sources) from
+# Read the HGNC gene data from the tab-delimited text file downloaded
+# from the HGNC website (www.genenames.org). Only "approved" entries
+# are retained.
+read_hgnc_data <- function (file) {
+  out <- suppressMessages(read_delim(file,delim = "\t"))
+  class(out) <- "data.frame"
+  names(out) <- c("hgnc_id","approved_symbol","approved_name","status",
+                  "previous_symbols","alias_symbols","chr","accession_numbers",
+                  "refseq_ids")
+  out <- transform(out,
+                   hgnc_id = as.numeric(sapply(strsplit(hgnc_id,":"),"[",2)),
+                   status  = factor(status))
+  return(subset(out,status == "Approved"))
+}
+
+# Read the pathway meta data (e.g., pathway names, data sources) from
 # the tab-delimited text file downloaded from the Pathway Commons
-# website.
-read_pc_pathways <- function (file) {
-  out <- suppressMessages(
-           read_delim(file,delim = "\t",col_names = TRUE,quote = ""))
+# website (https://www.pathwaycommons.org).
+read_pc_pathway_data <- function (file) {
+  out <- suppressMessages(read_delim(file,delim = "\t",quote = ""))
   class(out) <- "data.frame"
   names(out) <- tolower(names(out))
   out <- out[c("display_name","datasource","pathway_uri")]
--- scripts/prepare_pathway_data.R
+++ scripts/prepare_pathway_data.R
@@ -4,14 +4,20 @@ source("../code/read_data.R")
 
 # LOAD DATA
 # ---------
+# Read the HGNC (HUGO Gene Nomenclature Committee) gene data from the
+# tab-delimited text file.
+cat("Reading HGNC gene data from genes.txt.gz.\n")
+genes <- read_hgnc_data("../data/genes.txt.gz")
+
 # Read the Pathway Commons pathway meta data (e.g., pathway names,
 # data sources) from the tab-delimited text file.
 cat("Reading Pathway Commons pathway data from pathways.txt.gz.\n")
-out <- read_pc_pathways("../data/pathways.txt.gz")
+pathways <- read_pc_pathway_data("../data/pathways.txt.gz")
 
 # SUMMARIZE DATA
 # --------------
-print(summary(out$datasource))
+nrow(genes)
+print(summary(pathways$datasource))
 
 # WRITE DATA TO FILE
 # ------------------
