6557ef3ea287e4e06f8435510b7bacb540994daf
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-15 13:00:08 -0600

    Added placeholder roxygen2 docs for gene set data.

---
+++ R/gene_sets.R
@@ -0,0 +1,41 @@
+#' @name gene_sets_human
+#'
+#' @title Title Goes Here
+#'
+#' @docType data
+#'
+#' @description Add description here.
+#'
+#' @format \code{gene_sets_human} is a list with the following
+#'   list elements:
+#' 
+#' \describe{
+#'
+#'   \item{item}{Describe item here.}
+#' 
+#' }
+#'
+#' @keywords data
+#'
+NULL
+
+#' @name gene_sets_mouse
+#'
+#' @title Title Goes Here
+#'
+#' @docType data
+#'
+#' @description Add description here.
+#'
+#' @format \code{gene_sets_mouse} is a list with the following
+#'   list elements:
+#' 
+#' \describe{
+#'
+#'   \item{item}{Describe item here.}
+#' 
+#' }
+#'
+#' @keywords data
+#'
+NULL
