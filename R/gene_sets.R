54214d27a5fc493709c9b17c6598bdfa839634e4
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-15 21:32:20 -0600

    Working on roxygen2 docs for data sets; other improvements.

--- R/gene_sets.R
+++ R/gene_sets.R
@@ -1,19 +1,26 @@
 #' @name gene_sets_human
 #'
-#' @title Title Goes Here
+#' @title Human Gene Sets
 #'
 #' @docType data
 #'
-#' @description Add description here.
+#' @description 37,856 gene sets compiled from NCBI BioSystems,
+#'   Pathway Commons and MSigDB databases.
 #'
 #' @format \code{gene_sets_human} is a list with the following
 #'   list elements:
 #' 
 #' \describe{
 #'
-#'   \item{item}{Describe item here.}
-#' 
-#' }
+#'   \item{gene_info}{Data frame containing information on human
+#'     genes, including gene symbols and Ensembl ids.}
+#'
+#'   \item{gene_set_info}{Data frame contaiing information on gene
+#'     sets, including gene set name, id and database of origin.}
+#'
+#'   \item{gene_sets}{Gene sets encoded as a 61,676 x 37,856 sparse
+#'     matrix, in which gene_sets[i,j] = 1 if gene j belongs to gene set
+#'     i; otherwise, gene_sets[i,j] = 0.}}
 #'
 #' @keywords data
 #'
@@ -21,11 +28,12 @@ NULL
 
 #' @name gene_sets_mouse
 #'
-#' @title Title Goes Here
+#' @title Mouse Gene Sets
 #'
 #' @docType data
 #'
-#' @description Add description here.
+#' @description 37,856 mouse sets compiled from NCBI BioSystems,
+#'   Pathway Commons and MSigDB databases.
 #'
 #' @format \code{gene_sets_mouse} is a list with the following
 #'   list elements:
