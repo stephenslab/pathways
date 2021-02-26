361866959c91b1638aef90e05a436bbb0ff7728b
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-26 14:27:24 -0600

    Documented the pbmc_facs_z data set.

--- R/pbmc_facs_z.R
+++ R/pbmc_facs_z.R
@@ -1,13 +1,26 @@
 #' @name pbmc_facs_z
 #'
-#' @title Title Goes Here
+#' @title Differential Expression Results from PBMC Data
 #'
 #' @docType data
 #'
-#' @description Description goes here
+#' @description Results from differential expression analysis of
+#' single-cell RNA-seq profiles generated from bead-enriched
+#' subpopulations of peripheral blood mononuclear cell (PBMCs). The
+#' data were downloaded from
+#' \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets}.
 #'
-#' @format \code{pbmc_facs_z} is a matrix ...
+#' @format \code{pbmc_facs_z} is a matrix of z-scores, with rows
+#' corresponding to (human) genes and columns corresponding to
+#' differential expression analyses; differential expression was
+#' measured separately for each cell type (B cells, T cells, natural
+#' killer cells, CD14+ cells and CD34+ cells).
 #'
+#' @references
+#' Zheng et al (2017). Massively parallel digital transcriptional
+#' profiling of single cells. \emph{Nature Communications} \bold{8},
+#' 14049. \url{https://doi.org/10.1038/ncomms14049}
+#' 
 #' @keywords data
 #'
 NULL
