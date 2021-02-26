cd2f4b01d84bb99ce37e4f2022490f9dfc2cadbb
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-02-25 23:10:44 -0600

    I have a working, first-draft implementation of gsea_plotly.

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,22 @@
+gsea_plot_shapes <- c(23,24,21)
+gsea_plot_colors <- c("gold",         # biocyc
+                      "yellowgreen",  # C1
+                      "dodgerblue",   # C2
+                      "olivedrab",    # C3
+                      "firebrick",    # C4
+                      "darkorange",   # C5
+                      "magenta",      # C6
+                      "darkblue",     # C7
+                      "gold",         # H
+                      "tomato",       # humancyc
+                      "olivedrab",    # inoh
+                      "darkblue",     # kegg
+                      "royalblue",    # netpath
+                      "darkorange",   # panther
+                      "yellowgreen",  # pathbank
+                      "magenta",      # pid
+                      "dodgerblue")   # reactome
+
 #' @title Add Title Here
 #' 
 #' @description Create an interactive scatterplot using plotly to
@@ -7,6 +26,20 @@
 #' "perform_gsea"; and "label_gene_sets" is a vector of gene set ids
 #' to be labeled in the plot.
 #'
+#' @param gsea_res Describe input argument "gsea_res" here.
+#'
+#' @param gene_set_info Describe input argument "gene_set_info" here.
+#'
+#' @param k Describe input argument "k" here.
+#'
+#' @param margin Describe input argument "margin" here.
+#'
+#' @param height Describe input argument "height" here.
+#'
+#' @param width Describe input argument "width" here.
+#'
+#' @param title Describe input argument "title" here.
+#' 
 #' @return Describe return value here.
 #'
 #' @seealso \code{\link{perform_gsea}}
@@ -15,6 +48,8 @@
 #' # See perform_gsea for examples.
 #' 
 #' @importFrom plotly plot_ly
+#' @importFrom plotly add_trace
+#' @importFrom plotly layout
 #'
 #' @export
 #'
@@ -62,13 +97,14 @@ compile_data_for_gsea_plot <- function (gene_set_info, gsea_res, k,
   # Compute the "most extreme" signed p-value among other topics.
   n  <- nrow(P)
   p0 <- rep(0,n)
-  for (i in 1:n) {
+  rows <- which(!is.na(gsea_res$ES[,k]))
+  for (i in rows) {
     if (sign(gsea_res$ES[i,k]) > 0)
       p0[i] <- max(P[i,-k],na.rm = TRUE)
     else
       p0[i] <- min(P[i,-k],na.rm = TRUE)
   }
-  
+
   # Compile the data for plotting.
   dat <- data.frame(p1         = P[,k],
                     p0         = p0,
@@ -89,50 +125,9 @@ compile_data_for_gsea_plot <- function (gene_set_info, gsea_res, k,
   n   <- length(j)
   j   <- sample(j,ceiling(n/10))
   dat <- dat[c(i,j),]
-  
+
   # Set any missing signed p-values to zero.
   dat[is.na(dat$p1),"p1"] <- 0
   dat[is.na(dat$p0),"p0"] <- 0
   return(dat)
-}
-
-# Create an interactive scatterplot using plotly to explore the
-# gene-set enrichment results for a given topic k. Input argument
-# "gene_set_info" is a data frame containing information about the
-# gene sets; "gsea_res" is an output from function "perform_gsea"; and
-# "label_gene_sets" is a vector of gene set ids to be labeled in the
-# plot.
-create_gsea_plotly <- function (gene_set_info, gsea_res, k, margin = 1,
-                                height = 675, width = 800,
-                                title = paste("topic",k)) {
-
-  # Compile the data for plotting.
-  dat <- compile_data_for_gsea_plot(gene_set_info,gsea_res,k)
-  
-  # Create the plotly plot.
-  pmin <- min(c(dat$p0,dat$p1))
-  pmax <- max(c(dat$p0,dat$p1))
-  p <- plot_ly(data = dat,x = ~p0,y = ~p1,symbol = ~database,
-               color = ~collection,
-               text = ~sprintf("%s\nid: %s\np-value: %0.2e",name,id,10^(-p1)),
-               type = "scatter",mode = "markers",hoverinfo = "text",
-               symbols = gsea_plot_shapes,colors = gsea_plot_colors,
-               marker = list(size = 8,line = list(color = "white",width = 1)),
-               height = height,width = width)
-  p <- add_trace(p,data = data.frame(x = c(pmin,pmax),y = c(pmin,pmax)),
-                 x = ~x,y = ~y,mode = "lines",type = "scatter",
-                 inherit = FALSE,showlegend = FALSE,
-                 line = list(color = "lightgray",dash = "dash",size = 0.3))
-  p <- layout(p,
-              legend = list(font = list(size = 10)),
-              xaxis = list(title="most extreme signed p-value in other topics",
-                           showgrid = FALSE,showline = FALSE,zeroline = FALSE),
-              yaxis = list(title = "signed p-value in topic",
-                           showgrid = FALSE,showline = FALSE,zeroline = FALSE),
-              hoverlabel = list(bgcolor = "white",bordercolor = "black",
-                                font = list(color = "black",family = "arial",
-                                            size = 12)),
-              font = list(family = "arial",size = 12),
-              title = title)
-  return(p)
 }
